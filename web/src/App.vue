759946b45ad48ad99ba13c60a7acecfc0cc1d74b
Daniel Farr <daniel@aptivada.com>
2022-10-20 22:44:50 -0600

    ready to build pages

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import CenterH from "./components/layout/CenterH.vue";
 </script>
 
 <template>
-    <router-view></router-view>
+    <CenterH>
+        <router-view></router-view>
+    </CenterH>
 </template>
 
 <style scoped></style>
---
+++ web/src/components/layout/CenterH.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="center-h relative h-full w-full">
+        <div class="absolute max-h-full">
+            <div class="max-h-screen">
+                <div class="flex justify-center">
+                    <div>
+                        <slot></slot>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.absolute {
+    top: 50%;
+    right: 0;
+    left: 0;
+    transform: translateY(-50%);
+}
+
+.max-h-screen {
+    overflow-y: auto;
+}
+</style>
